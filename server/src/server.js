2257fcea81dde4288d80bbfaf8d96d0b80ea2685
Netrunner0101 <ericlung6572@gmail.com>
2022-09-18 02:02:33 +0200

    add mongodb/mongoose

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,12 @@
 const http = require('http');
 
 const app = require('./app');
+
+const mongoose = require('mongoose');
+
 const { loadPlanetData } = require('./routes/models/planets.model');
+
+const MONGO_URL = 'mongodb+srv://majesticjoe:LEGOMAN85207410@cluster0.aub34b3.mongodb.net/?retryWrites=true&w=majority';
 
 const PORT = process.env.PORT || 8000;
 
@@ -9,11 +14,17 @@ const server = http.createServer(app);
 
 // Be careful Not await outside function because not recognize PROMISE !!
 async function startServer(){
+
+    await mongoose.connect(MONGO_URL,{
+        
+    });
+
     await loadPlanetData();
 
     server.listen(PORT, ()=>{
         console.log(`Listening on port ${PORT}... `)
     });
+
 } 
 
 startServer();
